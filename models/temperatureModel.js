09bd8bc772b379d2f4e9676b399366a9da42bac7
Gulin <gulinisawesome@gmail.com>
2023-07-03 15:15:17 -0500

    yurr

--- models/temperatureModel.js
+++ models/temperatureModel.js
@@ -6,6 +6,7 @@ const temperatureSchema = mongoose.Schema(
             type:String,
             required:true,
         },
+        
         temperature:{
             type: Number,
             required:true,
