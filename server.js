b265c911e7b4357ff7ff301ef453c017417a61ec
Mau38 <maudiaz0109@gmail.com>
2023-07-03 15:02:32 -0500

    removed null and replaced it with *

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended : false}))
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'null');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   });
 
